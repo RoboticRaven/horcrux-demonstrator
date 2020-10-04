234368fd8a0529d3e89dc243835b4e196d4c213c
RoboticRaven <oliver.suzuki@gmail.com>
2020-10-04 14:22:42 -0700

    added scripted custom image creation, dockerhub push

---
+++ resources/nodeRed-customImage/push_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker login -u delirium
+
+# retagging of local images
+echo -e "retagging local images for push to cloud storage\n"
+docker tag hcd-bchain:latest delirium/horcrux-demonstrator:bchain
+docker tag hcd-bops:latest delirium/horcrux-demonstrator:bops
+docker tag hcd-identity-hub:latest delirium/horcrux-demonstrator:identity-hub
+docker tag hcd-issuer:latest delirium/horcrux-demonstrator:issuer
+docker tag hcd-mca:latest delirium/horcrux-demonstrator:mca
+docker tag hcd-verifier:latest delirium/horcrux-demonstrator:verifier
+
+# push all images
+echo -e "starting to push images - this may take a while\n"
+docker push delirium/horcrux-demonstrator:bchain
+docker push delirium/horcrux-demonstrator:bops
+docker push delirium/horcrux-demonstrator:identity-hub
+docker push delirium/horcrux-demonstrator:issuer
+docker push delirium/horcrux-demonstrator:mca
+docker push delirium/horcrux-demonstrator:verifier
