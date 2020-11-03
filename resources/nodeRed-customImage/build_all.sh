83ce9d5943dca2d18623a30ff1b42fb1b856d476
RoboticRaven <oliver.suzuki@gmail.com>
2020-11-03 07:04:42 -0800

    removed building artifacts for containers that are no longer necessary, also added node-red-contrib-ethr-genaddress module

--- resources/nodeRed-customImage/build_all.sh
+++ resources/nodeRed-customImage/build_all.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 
-cd bchain
-echo -e "buildling Node-RED image: hcd-bchain\n"
-./build.sh > build.log
-echo -e "build complete\n"
-
-cd ../bops
-echo -e "buildling Node-RED image: hcd-bops\n"
-./build.sh > build.log
-echo -e "build complete\n"
-
-cd ../identity-hub
-echo -e "buildling Node-RED image: hcd-identity-hub\n"
-./build.sh > build.log
-echo -e "build complete\n"
-
-cd ../issuer
-echo -e "buildling Node-RED image: hcd-issuer\n"
-./build.sh > build.log
-echo -e "build complete\n"
-
 cd ../mca
 echo -e "buildling Node-RED image: hcd-mca\n"
-./build.sh > build.log
-echo -e "build complete\n"
-
-cd ../verifier
-echo -e "buildling Node-RED image: hcd-verifier\n"
 ./build.sh > build.log
 echo -e "build complete\n"
 
--- resources/nodeRed-customImage/issuer/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t hcd-issuer:latest .
